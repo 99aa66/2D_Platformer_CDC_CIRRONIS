56926160fcf445809ac3460da24b26183b686e12
99aa66 <emanuelecirronis123@gmail.com>
2023-05-09 23:16:52 +0200

    Amelioration_du_PickAndThrow

--- Assets/PickUpAndThrow.cs
+++ Assets/PickUpAndThrow.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PickUpAndThrow : MonoBehaviour
@@ -9,11 +10,15 @@ public class PickUpAndThrow : MonoBehaviour
     private Rigidbody2D rb2d;
     public Transform grabCheck;
     public LayerMask GrabLayer; // couche du sol
+    public Transform groundCheck; // objet qui vérifie si le joueur touche le sol
+
     private bool isPick;
-    private FollowObject Follow;
+    private Follow Follow;
+    [SerializeField] private bool isGrounded = false;
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        Follow = GetComponent<Follow>();
     }
 
     void Update()
@@ -27,13 +32,20 @@ public class PickUpAndThrow : MonoBehaviour
         {
             Throw(); 
         }
+        else
+        {
+            rb2d.gravityScale = 1;
+            if (isGrounded == true)
+            {
+                Follow.enabled = true;
+                transform.parent = null;
+            }
+        }
     }
 
     void PickUp()
     {
-        rb2d.isKinematic = true;
-        //rb2d.gravityScale = 0;
-        Follow = gameObject.GetComponent<FollowObject>();
+        rb2d.gravityScale = 0;
         Follow.enabled = false;
         transform.parent = grabCheck.transform; 
         transform.localPosition = new Vector2(0.5f, 0.5f); 
@@ -43,11 +55,14 @@ public class PickUpAndThrow : MonoBehaviour
 
     void Throw()
     {
-        rb2d.isKinematic = false;
-        rb2d.AddForce(grabCheck.transform.forward * throwForce, ForceMode2D.Impulse);
+        transform.parent = null;
+        rb2d.velocity = transform.right * throwForce;
         //rb2d.AddForce(grabCheck.transform.up * throwForce, ForceMode2D.Impulse);
         isPickedUp = false;
     }
-
-
+    void FixedUpdate()
+    {
+        // vérifie si le joueur touche le sol
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, GrabLayer);
+    }
 }
